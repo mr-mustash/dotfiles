4878fd971c336c793d21a075572eb46f7c49aa1d
Patrick King <pking@newrelic.com>
2018-11-01 15:56:05 -0700

    Making it so that when I run kubectl it desplays the current context on the right hand side of my prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -29,11 +29,40 @@ function __connection_type
   end
 end
 
+function __kubectl_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color normal)"no config"
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color normal)"no context"
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo " "(set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color normal)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
+end
+
+
 function fish_right_prompt -d 'Setting the right prompt with connection type or command duration'
   set_color $fish_color_redirection
+  set -gx __date (date +%s)
+
+  if set -q __kubectl_run
+    if test (math "$__date - $__kubectl_run") -lt 300
+        __kubectl_status
+    end
+  end
+
   __connection_type
 
   __cmd_duration
-
 end
-
