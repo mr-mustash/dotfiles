5c5922528f7d71370e7b39266d2601795f18aa02
Patrick King <pking@newrelic.com>
2019-08-07 16:18:07 -0700

    More prompt time. - Moving all `__*.fish` files into their own function directory - Making my prompt display my git status with icons

---
+++ .config/fish/prompt/prompt_functions/__fish_right_prompt_k8s_context.fish
@@ -0,0 +1,21 @@
+function __fish_right_prompt_k8s_context -d "Show the kubectl current context after running the `k` alias"
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color normal)"no config "
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color normal)"no context "
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color normal)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns) "
+end
