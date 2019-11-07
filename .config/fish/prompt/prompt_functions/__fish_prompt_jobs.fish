f29d8564418673fb7916d750b3f871cadfd4a040
Patrick King <pking@newrelic.com>
2019-11-07 15:40:09 -0800

    Updating submodules

--- .config/fish/prompt/prompt_functions/__fish_prompt_jobs.fish
+++ .config/fish/prompt/prompt_functions/__fish_prompt_jobs.fish
@@ -1,7 +1,7 @@
 function __fish_prompt_jobs --description 'Helper function for fish_prompt'
     jobs | egrep -v 'autojump' >/dev/null 2>/dev/null; or return 0
 
-    set job_count (jobs | egrep -v 'autojump|__fish_prompt_git_status' | wc -l)
+    set job_count (jobs | egrep -v 'autojump|__fish_prompt_git_status|-WINCH' | wc -l)
 
     if test "$job_count" != "0"
         echo -n (set_color $fish_prompt_color_jobs) "$job_count"
