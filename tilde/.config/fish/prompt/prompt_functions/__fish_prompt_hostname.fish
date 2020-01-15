0039ababc72b007f99fcfc0085c1446819f2c2e7
Patrick King <patrick@patrickcking.com>
2020-01-14 19:19:20 -0800

    Fixing hostname prompt My hostname prompt was very fragile and broke in a lot of conditions. This seeks to remedy that.

--- tilde/.config/fish/prompt/prompt_functions/__fish_prompt_hostname.fish
+++ tilde/.config/fish/prompt/prompt_functions/__fish_prompt_hostname.fish
@@ -1,13 +1,10 @@
 function __fish_prompt_hostname -d "Display hostname on remote hosts"
-    if not set -q short_hostname
-        set -g short_hostname (uname -n | string match -r "[^.]+")
-        if test (awk -F/ '$2 == "docker"' /proc/self/cgroup | read) != ""
-            set -g short_hostname $short_hostname(echo -ns "(")
-            set -g short_hostnaem $short_hostname(echo -ns (set_color $fish_prompt_color_docker_icon) "" (set_color normal))
-            set -g short_hostname $short_hostname (echo -ns ") ")
+    if test "$SSH_CONNECTION" != "" || test "$in_docker" != ""
+        if not set -q short_hostname
+            set -g short_hostname (uname -n | string match -r "[^.]+")
         end
-    end
 
-    echo -ns (set_color $fish_color_host) "$short_hostname " (set_color normal)
-    echo -ns (set_color --bold) "in " (set_color normal)
+        echo -ns (set_color $fish_color_host) "$short_hostname$in_docker " (set_color normal)
+        echo -ns (set_color --bold) "in " (set_color normal)
+    end
 end
