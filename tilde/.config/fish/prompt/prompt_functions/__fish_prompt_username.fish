120e2dbb3941e0441b2412b6a2424a23a7524f6c
Patrick King <patrick@patrickcking.com>
2020-01-14 19:48:21 -0800

    More username prompt fixing.

--- tilde/.config/fish/prompt/prompt_functions/__fish_prompt_username.fish
+++ tilde/.config/fish/prompt/prompt_functions/__fish_prompt_username.fish
@@ -14,10 +14,10 @@ function __fish_prompt_username --description 'Display username on remost host o
             set user_color $fish_color_user
         end
 
-        if test "$SSH_CONNECTION" != ""
+        if test "$SSH_CONNECTION" != "" -o "$in_docker" != ""
             # Only need "@" if we're connected remotely
-            set _local_user (echo -ns "$USER" ; set_color normal ; echo -ns "@")
-            # Don't display "in" since this will be taken care of by __fish_prompt_hostname when remote
+            set _local_user (echo -ns "$USER" ; set_color normal ; set_color --bold ; echo -ns " via ")
+            # Don't display "via" since this will be taken care of by __fish_prompt_hostname when remote
             echo -ns (set_color $user_color) "$_local_user" (set_color normal)
         else
             echo -ns (set_color $user_color) "$_local_user " (set_color normal)
