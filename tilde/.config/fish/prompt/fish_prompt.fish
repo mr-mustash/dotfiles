7c8ce843dc1d018fe95958ac75b9dd5c6f89fc01
Patrick King <pking@newrelic.com>
2020-01-27 10:28:46 -0800

    Quality of Life Update
    After moving to use this repo as my complete source of truth of my
    dotfiles there were lots of cases where I had just been editing things
    by hand to stop errors. This update removes a lot of those errors, as
    well as adds so new functionality at the same time.
    
    Dear future Patrick, I'm sorry about how much is in this one commit, but
    please forgive me. I'll do better about making branches for each
    feature. I promise.

--- tilde/.config/fish/prompt/fish_prompt.fish
+++ tilde/.config/fish/prompt/fish_prompt.fish
@@ -14,6 +14,20 @@ function fish_prompt --description 'Write out the prompt'
     __fish_prompt_languages
     __fish_prompt_jobs
 
-    set -l glyph "➜"
-    echo -ns (set_color $fish_color_user) " $glyph " (set_color normal)
+    # Change the color of the glyph based on vi mode
+    switch $fish_bind_mode
+        case default
+            # bryellow is equal to grey???
+            set fish_color_prompt bryellow --bold
+        case insert
+            # This is different than the yellow that I use
+            # for Vim but it's much more aestheticlaly pleasing.
+            set fish_color_prompt $fish_color_user
+        case visual
+            set fish_color_prompt magenta --bold
+    end
+
+    #set -l glyph "➜"
+    set -l glyph "❯"
+    echo -ns (set_color $fish_color_prompt) " $glyph " (set_color normal)
 end
