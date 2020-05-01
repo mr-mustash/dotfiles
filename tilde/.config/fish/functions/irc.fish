0bf02831b0638b271a70fbbe24156237fa2aa300
Patrick King <pking@newrelic.com>
2020-05-01 15:52:10 -0700

    New fish functions - fssh: ssh to a host and pull down the latest version of my homedir   docker container. - irc: Start a weechat screen session, or enter an already running one.

---
+++ tilde/.config/fish/functions/irc.fish
@@ -0,0 +1,8 @@
+function irc -d "Connect to or start a screen session running weechat"
+    screen -list | grep irc > /dev/null 2>1
+    if test $status -eq 0
+        screen -r irc
+    else
+        screen -S irc -m weechat
+    end
+end
