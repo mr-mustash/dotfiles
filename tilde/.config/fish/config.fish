36f8b7aa6736f056c764e31515e9dcfe8fdafc52
Patrick King <patrick@patrickcking.com>
2020-05-24 10:31:53 -0700

    WIP: Rebuilding async prompt
    After fish_prompt in my [last
    commit](https://github.com/mr-mustash/dotfiles/commit/fc923bb84303146cb5320a2056a1d11c658b0b0d)
    I started to wonder if I was able to test out
    [acomagu/fish-async-prompt](https://github.com/acomagu/fish-async-prompt)
    again. I originally didn't use this script because I was unable to get
    it work with how I had everything set up. Now, with just a couple of
    changes to my prompt, it's working pretty great!
    
    This ticket is still WIP because I don't know the performance
    implications yet, or if it's going to affect my workflow. I'm going to
    keep this branch up with Master, and try to switch back and forth to see
    if I can get a feel for acomagu/fish-async-prompt's performance in the
    real world.

--- tilde/.config/fish/config.fish
+++ tilde/.config/fish/config.fish
@@ -3,12 +3,20 @@ if status is-interactive
     source "$__fish_config_dir/colors.fish"
 
     # Adding my prompt configs first
+
+    #source $HOME/.config/fish/functions/__update_prompt.fish
     set -gx fish_function_path $HOME/.config/fish/prompt/prompt_functions/ $fish_function_path
     set -gx fish_function_path $HOME/.config/fish/prompt/ $fish_function_path
+
+    #set -g async_prompt_inherit_variables all
+    set -g async_prompt_functions __fish_prompt_git_status __fish_prompt_git_autofetch
 
     # Key bindings
     test -e $HOME/.config/fish/function/fish_user_key_bindings.fish; and set fish_key_bindings fish_user_key_bindings
 
     # Load iTerm 2 shell integration
     test -e {$HOME}/.config/fish/functions/iterm_shell_integration.fish; and source {$HOME}/.config/fish/functions/iterm_shell_integration.fish
+else
+    set -gx fish_function_path $HOME/.config/fish/prompt/prompt_functions/ $fish_function_path
+    set -gx fish_function_path $HOME/.config/fish/prompt/ $fish_function_path
 end
--- tilde/.config/fish/prompt/prompt_functions/__fish_prompt_git_status.fish
+++ tilde/.config/fish/prompt/prompt_functions/__fish_prompt_git_status.fish
@@ -1,19 +1,92 @@
 function __fish_prompt_git_status --description 'Display git info in the fish prompt'
-    if not set -q __git_last_dirty_check
-        set -g __git_last_dirty_check 0
-    end
+    set -l dirty_file_count (git status --porcelain -b 2>/dev/null | wc -l)
+    if test $dirty_file_count -gt 0
+        set -l icon_untracked "?"
+        set -l icon_added "+"
+        set -l icon_modified "~"
+        set -l icon_renamed "»"
+        set -l icon_deleted "-"
+        set -l icon_stashed "\$"
+        set -l icon_unmerged "<>"
+        set -l icon_diverged "↕"
+        set -l icon_ahead "⇡"
+        set -l icon_behind "⇣"
 
-    set -l current_timestamp (command date +%s)
-    set -l time_since_last_dirty_check (math "$current_timestamp - $__git_last_dirty_check")
+        set -l icon_clean "√"
 
-    if test $time_since_last_dirty_check -gt 2
-        # This is here because I'm using `-b` to find dirty branches
-        # which means there will always be at least one line here
-        # even when the branch is clean.
-        # IF THINGS ARE BROKEN CHECK HERE FIRST BECAUSE IT'S ALWAYS
-        # THIS GODDAMNED COMMAND AND IT'S EDGE CASES
-        set -l cmd "git status --porcelain -b 2>/dev/null | wc -l"
-        __run_async "__checking_dirty" __fish_prompt_git_dirty_callback $cmd
+        set -l GIT_PROMPT_ORDER untracked added modified renamed deleted stashed unmerged diverged ahead behind
+
+        set -l index (git status --porcelain 2>/dev/null -b)
+        set -l trimmed_index (string split \n $index | string sub --start 1 --length 2)
+
+
+
+        for i in $trimmed_index
+            if test (string match '\?\?' $i)
+                set git_status untracked $git_status
+            end
+            if test (string match '*A*' $i)
+                set git_status added $git_status
+            end
+            if test (string match '*M*' $i)
+                set git_status modified $git_status
+            end
+            if test (string match '*R*' $i)
+                set git_status renamed $git_status
+            end
+            if test (string match '*D*' $i)
+                set git_status deleted $git_status
+            end
+            if test (string match '*U*' $i)
+                set git_status unmerged $git_status
+            end
+        end
+
+        # Check for stashes
+        if test -n (echo (command git rev-parse --verify refs/stash 2>/dev/null))
+            set git_status stashed $git_status
+        end
+
+        # Check whether the branch is ahead
+        if test (string match '*ahead*' $index)
+            set is_ahead true
+        end
+
+        # Check whether the branch is behind
+        if test (string match '*behind*' $index)
+            set is_behind true
+        end
+
+        # Check whether the branch has diverged
+        if test "$is_ahead" = "true" -a "$is_behind" = "true"
+            set git_status diverged $git_status
+        else if test "$is_ahead" = "true"
+            set git_status ahead $git_status
+        else if test "$is_behind" = "true"
+            set git_status behind $git_status
+        end
+
+        for i in $GIT_PROMPT_ORDER
+            if contains $i in $git_status
+                set -l status_color (echo fish_prompt_color_git_$i)
+                set -l full_color "$$status_color"
+
+                set -l status_symbol icon_$i
+                set -l full_status_symbol "$$status_symbol"
+                set -l status_with_color (set_color $full_color --bold; echo $full_status_symbol ; set_color normal)
+
+                set full_git_status "$full_git_status$status_with_color"
+
+            end
+        end
+
+        if test "$full_git_status" != ""
+            set -g __git_is_dirty (echo -ns "[$full_git_status]")
+        else
+            set -e __git_is_dirty
+        end
+    else
+        set -e __git_is_dirty
     end
 
     if set -q __git_is_dirty
