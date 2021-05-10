d08adb5d578e2230ef154e42d1491eed2007888a
Patrick King <patrick@patrickcking.com>
2021-05-10 13:19:01 -0700

    Bulk Updates - Adding in Firefox user-overrides for github.com/arkenfox/user.js - Updated Brewfile - Setting up aliases to use 'main' as the preferred branch over 'master' - Fixing prompt for Fish 3.2 - Adding in updated gpg.conf after redoing my whole gpg stack

---
+++ Applications/Firefox/user-overrides.js
@@ -0,0 +1,13 @@
+/*** MY OVERRIDES ***/
+user_pref("_user.js.parrot", "overrides section syntax error");
+user_pref("network.http.referer.XOriginPolicy", 2);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+user_pref("keyword.enabled", true);
+/*** Below is from https://www.youtube.com/watch?v=xxWXLlfqNAo&t=307s ***/
+user_pref("security.ssl3.rsa_des_ede3_sha", false);
+user_pref("browser.cache.disk_cache_ssl", false);
+user_pref("browser.cache.memory.enable", false);
+user_pref("geo.enabled", false);
+user_pref("plugin.scan.plid.all", false);
+user_pref("security.ssl.enable_false_start", false);
+user_pref("_user.js.parrot", "SUCCESS");
