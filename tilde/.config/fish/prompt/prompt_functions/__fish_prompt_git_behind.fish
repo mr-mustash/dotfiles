1ec9ac8f2f9f17061b56a27b014a51fcc2cde9e4
Patrick King <patrick@patrickcking.com>
2022-07-08 12:19:44 -0700

    Resolving conflits

--- tilde/.config/fish/prompt/prompt_functions/__fish_prompt_git_behind.fish
+++ tilde/.config/fish/prompt/prompt_functions/__fish_prompt_git_behind.fish
@@ -1,9 +1,13 @@
 function __fish_prompt_git_behind
-    set -l _branch_updates (git rev-list --no-merges --count @{upstream}...@)
-    if test $_branch_updates -gt 0
-        echo -ns (set_color normal) "("
-        echo -ns (set_color $fish_prompt_color_git_behind) "" (set_color normal)
-        echo -ns " $_branch_updates"
-        echo -ns ")"
+    # Testing to see if a remote branch exists. This is important because in
+    # __fish_prompt_git_, we only check for the existence of a local branch.
+    if test (command git rev-parse --abbrev-ref @'{u}' 2>/dev/null)
+        set -l _branch_updates (git rev-list --no-merges --count @{upstream}...@)
+        if test $_branch_updates -gt 0
+            echo -ns (set_color normal) "("
+            echo -ns (set_color $fish_prompt_color_git_behind) "" (set_color normal)
+            echo -ns " $_branch_updates"
+            echo -ns ")"
+        end
     end
 end
