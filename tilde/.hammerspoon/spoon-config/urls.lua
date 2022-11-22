041f5b2044b5d378b36c62b83338c4f8b559a176
Patrick King <patrick@patrickcking.com>
2022-11-21 17:37:52 -0800

    Fixup for merging main back in to my local branch.

--- tilde/.hammerspoon/spoon-config/urls.lua
+++ tilde/.hammerspoon/spoon-config/urls.lua
@@ -7,23 +7,22 @@ function urls.init()
 
     local builtin = appID(secrets.urls.builtin)
     local default = appID(secrets.urls.default)
-    --local videoPlayer = appID(secrets.urls.videoPlayer)
-    --local meetings = appID(secrets.urls.meetings)
+    if Local.env == "work" then
+        local meetings = appID(secrets.urls.meetings)
+    else
+        local meetings = appID(secrets.urls.default)
+    end
 
     spoon.URLDispatcher.default_handler = default
     spoon.URLDispatcher.decode_slack_redir_urls = true
     spoon.URLDispatcher.set_system_handler = true
 
     spoon.URLDispatcher.url_patterns = {
-        --{"https?://%w+.youtube.com/watch?v=", videoPlayer},
-        --{"https?://www.youtube.com/watch?v=", videoPlayer},
-        --{"https?://youtu.be/", videoPlayer},
         {"https://captive.apple.com/", builtin},
         {"https?://%w+.beatsense.com", builtin},
         {"https?://%w+.twitter.com/", builtin},
         {"https?://%w+.zoom.us/j/", meetings},
         {"https?://%w+w2g.tv", builtin},
-        {"https?://twitter.com/", builtin},
         {"https?://twitter.com/", builtin},
         {"https?://w2g.tv", builtin},
         {"https?://www.beatsense.com", builtin},
