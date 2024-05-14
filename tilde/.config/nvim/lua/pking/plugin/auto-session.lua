69381cfa0d63cf459cb5abd16d6cba8e35a602ec
mr-mustash <patrick@patrickcking.com>
2024-05-14 10:29:57 -0700

    New Laptop New Problems
    Setting up a new laptop and fixing errors.

--- tilde/.config/nvim/lua/pking/plugin/auto-session.lua
+++ tilde/.config/nvim/lua/pking/plugin/auto-session.lua
@@ -5,6 +5,9 @@ return {
             auto_session_enabled = true,
             auto_session_create_enabled = true,
             auto_session_use_git_branch = true,
+            auto_session_enable_last_session = true,
+            auto_save_enabled = true,
+            auto_restore_enabled = true,
             auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Library", "/"},
 
 
